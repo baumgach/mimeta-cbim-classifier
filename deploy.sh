f4041953772d341887969020aed73dfdcad2b74d
Christian Baumgartner <c.f.baumgartner@gmail.com>
2023-07-25 00:00:20 +0200

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -24,7 +24,7 @@ conda activate pytorch
 
 # Run our code
 echo "-------- PYTHON OUTPUT ----------"
-python train.py --name="cbim" --use_data_augmentation
+python train.py --experiment_name="cbim" --use_data_augmentation
 python test.py --checkpoints_dir="runs/cbim-with-aug/version_0/checkpoints" 
 echo "---------------------------------"
 
